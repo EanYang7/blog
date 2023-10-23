9f3d6a320a8de1f8066d10cdf30f7106723055cb
YQisme <1398757912@qq.com>
2023-10-23 14:22:18 +0800

    因为重新设置了baseurl，所以原来的图片路径/前缀改为/blog

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -379,7 +379,7 @@ function TodayCard({ cRef }) {
           } cursor-pointer today-card-cover absolute w-full h-full top-0`}
           style={{
             background:
-              "url('https://bu.dusays.com/2023/03/12/640dcd3a1b146.png') no-repeat center /cover"
+              "url('/blog/images/heo/653AB13D32B92D75C6E745D7DF8FFBBD.jpg') no-repeat center /cover"
           }}
         ></div>
       </div>
